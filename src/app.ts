d51bfed959d9a47616606246c779f155a71f2a29
Jerome Thoma <thoma352>
2026-04-21 19:10:04 +0200

    cleanup

--- src/app.ts
+++ src/app.ts
@@ -94,14 +94,3 @@ await routingDataService.loadRegionData("ellergronn")
 
 // All ready
 appBus.emit("system:ready")
-
-// Experimental
-if (navigator.storage && navigator.storage.estimate) {
-    const quota = await navigator.storage.estimate();
-    // quota.usage -> Number of bytes used.
-    // quota.quota -> Maximum number of bytes available.
-    const percentageUsed = (quota.usage / quota.quota) * 100;
-    console.log(`You've used ${percentageUsed}% (${quota.usage/1024}kb) of the available storage.`);
-    const remaining = quota.quota - quota.usage;
-    console.log(`You can write up to ${remaining} more bytes.`);
-}
